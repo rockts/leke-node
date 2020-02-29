f352701fafd3846ee8067bcdd4ad13e5273bc3d2
rockts <rockts@sina.com>
2020-03-01 03:42:17 +0800

    FS：del

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const fs = require("fs");
 
-fs.rename("logs/hello.log", "logs/greeting.log", error => {
+fs.readdirSync("logs").map(file => {
+  fs.unlink(`logs/${file}`, error => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(`成功的删除了文件：${file}`);
+    }
+  });
+});
+
+fs.rmdir("logs", error => {
   if (error) {
     console.log(error);
   } else {
-    console.log("重命名成功");
+    console.log("成功的删除了目录：logs");
   }
 });
