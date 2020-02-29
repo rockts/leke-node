f2f27714bba11d4b7a7c0c811a288562fd74fa75
rockts <rockts@sina.com>
2020-03-01 03:23:56 +0800

    FS：readFile

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 const fs = require("fs");
 
-fs.writeFile("logs/hello.log", "您好 ~ \n", error => {
+fs.readFile("logs/hello.log", "utf8", (error, data) => {
   if (error) {
     console.log(error);
   } else {
-    console.log("成功写入文件");
-  }
-});
-
-fs.appendFile("logs/hello.log", "hello ~ \n", error => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("成功写入文件");
+    console.log(data);
   }
 });
