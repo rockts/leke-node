5e746945bdf2de50d7dc3159c8e6b2e5d3134026
rockts <rockts@sina.com>
2020-03-01 03:05:21 +0800

    Event

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hahahah ");
+const EventEmitter = require("events");
+
+class Player extends EventEmitter {}
+
+var player = new Player();
+
+player.once("play", track => {
+  console.log(`正在播放： 《${track}》`);
+});
+
+player.emit("play", "再见理想");
+player.emit("play", "海阔天空");
