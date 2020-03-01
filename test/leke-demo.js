13a316ac649e599c38af9379fdaafdaf0122ebcf
rockts <rockts@sina.com>
2020-03-02 00:38:22 +0800

    测试方法的异常

--- test/leke-demo.js
+++ test/leke-demo.js
@@ -5,12 +5,23 @@ const LekeDemo = require("../lib/leke-demo");
 var demo = new LekeDemo();
 
 describe("LekeDemo", function() {
-  it("加载豆瓣 API 返回的内容里应该包含 subjects 属性", function(done) {
-    demo.fetchData("top250", function(data) {
-      expect(data).to.have.property("subjects");
-      done();
-    });
+  it("给汽车引擎加水是不能接受的事情", function() {
+    expect(demo.engine.bind(demo, "water")).to.throw("not accept");
   });
+
+  // it("给汽车引擎加水是不能接受的事情", function() {
+  //   expect(function() {
+  //     demo.engine("water");
+  //   }).to.throw("not accept");
+  // });
+
+  // it("加载豆瓣 API 返回的内容里应该包含 subjects 属性", function(done) {
+  //   demo.fetchData("top250", function(data) {
+  //     expect(data).to.have.property("subjects");
+  //     done();
+  //   });
+  // });
+
   // this.timeout(5000);
 
   // it("一段时间以后返回数据", function(done) {
