af2f85d6e0bb7ee4959b612de938c9ad5d53d621
rockts <rockts@sina.com>
2020-03-02 02:37:32 +0800

    提交评论

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express, { request } from "express";
 import morgan from "morgan";
 import path from "path";
+import bodyParser from "body-parser";
 
 const app = express();
 
 app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+let comments = [];
+app.locals.comments = comments;
 
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -15,6 +20,18 @@ app.get("/", (request, response) => {
 
 app.get("/comments/new", (request, response) => {
   response.render("comments/new");
+});
+
+app.post("/comments/new", (request, response) => {
+  if (!request.body.comment) {
+    response.status(400).send("Do you have something to say ？");
+    return;
+  }
+  comments.push({
+    comment: request.body.comment,
+    created_at: new Date()
+  });
+  response.redirect("/comments");
 });
 
 app.listen(3000, () => {
