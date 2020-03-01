f304c73a714e90fe181b63a1f40d513eb237acd4
rockts <rockts@sina.com>
2020-03-02 02:20:19 +0800

    Views: 视图

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 import express, { request } from "express";
 import morgan from "morgan";
+import path from "path";
 
 const app = express();
 
 app.use(morgan("dev"));
 
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.get("/", (request, response) => {
-  response.send("hello ~");
+  response.render("index");
 });
 
 app.listen(3000, () => {
