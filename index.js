46cf77ec608832ddcfe81f1b8e18a76b753945ed
rockts <rockts@sina.com>
2020-03-02 02:13:56 +0800

    Middleware：中间件

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express, { request } from "express";
+import morgan from "morgan";
 
 const app = express();
+
+app.use(morgan("dev"));
 
 app.get("/", (request, response) => {
   response.send("hello ~");
