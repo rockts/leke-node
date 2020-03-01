b3660435eb74e5ff46f99ca8097748797b0e5377
rockts <rockts@sina.com>
2020-03-01 23:13:01 +0800

    chai 断言测试

--- test/leke-demo.js
+++ test/leke-demo.js
@@ -1,20 +1,11 @@
+const chai = require("chai");
+const assert = chai.assert;
+
 describe("LekeDemo", function() {
-  describe("方法 1", function() {
-    before(function() {
-      console.log("--- 测试之前");
-    });
-    after(function() {
-      console.log("--- 测试之后");
-    });
-    beforeEach(function() {
-      console.log("------ 每条测试之前");
-    });
-    afterEach(function() {
-      console.log("------ 每条测试之后");
-    });
-    context("情境 1", function() {
-      it("方法 1", function() {});
-      it("方法 2", function() {});
-    });
+  it("使用 assert 风格的断言测试", function() {
+    var value = "hello";
+    assert.typeOf(value, "string");
+    assert.equal(value, "hello");
+    assert.lengthOf(value, 5);
   });
 });
