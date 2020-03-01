2d027c625e258176fe679d06ee831631f8b0415c
rockts <rockts@sina.com>
2020-03-02 02:04:04 +0800

    创建一个应用

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+import express, { request } from "express";
 
+const app = express();
+
+app.get("/", (request, response) => {
+  response.send("hello ~");
+});
+
+app.listen(3000, () => {
+  console.log("Listen port: 3000");
+});
