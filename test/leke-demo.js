8b9aab3ac3c839a7bbcfd6cf72313c3da5c584ef
rockts <rockts@sina.com>
2020-03-02 00:27:00 +0800

    异步；http

--- test/leke-demo.js
+++ test/leke-demo.js
@@ -5,14 +5,20 @@ const LekeDemo = require("../lib/leke-demo");
 var demo = new LekeDemo();
 
 describe("LekeDemo", function() {
-  this.timeout(5000);
-
-  it("一段时间以后返回数据", function(done) {
-    demo.waitTwoSecond("hello", function(data) {
-      expect(data).to.equal("hello");
+  it("加载豆瓣 API 返回的内容里应该包含 subjects 属性", function(done) {
+    demo.fetchData("top250", function(data) {
+      expect(data).to.have.property("subjects");
       done();
     });
   });
+  // this.timeout(5000);
+
+  // it("一段时间以后返回数据", function(done) {
+  //   demo.waitTwoSecond("hello", function(data) {
+  //     expect(data).to.equal("hello");
+  //     done();
+  //   });
+  // });
   // it("单价是 10 块钱的 3 件商品小计金额应该是 30 块", function() {
   //   var subtotal = demo.subtotal(10, 3);
   //   expect(subtotal).to.equal(30);
