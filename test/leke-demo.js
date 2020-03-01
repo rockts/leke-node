b2498f5e0cda004eed395d02d8e7d1113716ca62
rockts <rockts@sina.com>
2020-03-02 00:06:51 +0800

    异步；setTimeout

--- test/leke-demo.js
+++ test/leke-demo.js
@@ -5,8 +5,16 @@ const LekeDemo = require("../lib/leke-demo");
 var demo = new LekeDemo();
 
 describe("LekeDemo", function() {
-  it("单价是 10 块钱的 3 件商品小计金额应该是 30 块", function() {
-    var subtotal = demo.subtotal(10, 3);
-    expect(subtotal).to.equal(30);
+  this.timeout(5000);
+
+  it("一段时间以后返回数据", function(done) {
+    demo.waitTwoSecond("hello", function(data) {
+      expect(data).to.equal("hello");
+      done();
+    });
   });
+  // it("单价是 10 块钱的 3 件商品小计金额应该是 30 块", function() {
+  //   var subtotal = demo.subtotal(10, 3);
+  //   expect(subtotal).to.equal(30);
+  // });
 });
