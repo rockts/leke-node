c8d68a95e0644e123794120a2c11257dfa32128c
rockts <rockts@sina.com>
2020-03-10 04:56:59 +0800

    上传单个文件

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const app = express();
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
+app.post("/profile", upload.single("avatar"), (request, response, next) => {
+  response.send(request.file);
+});
 
 app.listen(8080, () => {
   console.log("localhost:8080");
