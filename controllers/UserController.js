92515a511afea5370cc1989543dcc17fe0d92ec8
rockts <rockts@sina.com>
2020-03-10 03:39:48 +0800

    注册用户

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,18 @@
+const User = require("../models/user");
+
 const store = (request, response) => {
-  response.send("注册用户");
+  const userName = request.body.userName;
+  const password = request.body.password;
+
+  const user = new User({
+    userName,
+    password
+  });
+
+  user
+    .save()
+    .then(() => response.send({ message: "注册成功" }))
+    .catch(error => response.send(error));
 };
 
 module.exports = {
