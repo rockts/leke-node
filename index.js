fe316da3cb827646f995e8cf31b06162c42aa2c5
rockts <rockts@sina.com>
2020-03-10 00:44:00 +0800

    创建与签发 JWT

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hahahah ");
+const jwt = require("jsonwebtoken");
+
+const payload = {
+  name: "gaopeng",
+  admin: true
+};
+
+const secret = "I_LOVE_LEKE";
+
+const token = jwt.sign(payload, secret);
+console.log(token);
