17b6a09c2f9cf687c4d1cf934b1c9e01fbef11fe
rockts <rockts@sina.com>
2020-03-10 02:45:59 +0800

    用 RS256 算法签发 JWT

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 const jwt = require("jsonwebtoken");
+const fs = require("fs");
 
 const payload = {
   name: "gaopeng",
   admin: true
 };
 
-const secret = "I_LOVE_LEKE";
+/**
+ * HS256
+ */
 
-const token = jwt.sign(payload, secret);
+// const secret = "I_LOVE_LEKE";
+
+// const token = jwt.sign(payload, secret);
+// console.log(token);
+
+// jwt.verify(token, secret, (error, decoded) => {
+//   if (error) {
+//     console.log(error.message);
+//     return;
+//   }
+
+//   console.log(decoded);
+// });
+
+/**
+ * RS256
+ */
+
+const privateKey = fs.readFileSync("./config/private.key");
+const token = jwt.sign(payload, privateKey, { algorithm: "RS256" });
 console.log(token);
-
-jwt.verify(token, secret, (error, decoded) => {
-  if (error) {
-    console.log(error.message);
-    return;
-  }
-
-  console.log(decoded);
-});
