6edc76b9c5e0bb7f960586b9bc47238d70a12f25
rockts <rockts@sina.com>
2020-03-10 05:13:11 +0800

    文件上传过滤器

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+
+const fileFilter = (request, file, callback) => {
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    return callback(new Error("images only :)"), false);
+  }
+  callback(null, true);
+};
+
+const upload = multer({ dest: "uploads/", fileFilter });
 
 app.post("/profile", upload.single("avatar"), (request, response, next) => {
   response.send(request.file);
@@ -14,6 +22,12 @@ app.post(
     response.send(request.files);
   }
 );
+
+app.use((error, request, response, next) => {
+  response.status(500).send({
+    message: error.message
+  });
+});
 
 app.listen(8080, () => {
   console.log("localhost:8080");
