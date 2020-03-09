9b1f6b96e47790ba7b95654557f1202e6fd81870
rockts <rockts@sina.com>
2020-03-10 03:55:57 +0800

    存储 hash 之后的密码

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,18 +1,20 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 const store = (request, response) => {
   const userName = request.body.userName;
-  const password = request.body.password;
 
-  const user = new User({
-    userName,
-    password
+  bcrypt.hash(request.body.password, 10).then(password => {
+    const user = new User({
+      userName,
+      password
+    });
+
+    user
+      .save()
+      .then(() => response.send({ message: "注册成功" }))
+      .catch(error => response.send(error));
   });
-
-  user
-    .save()
-    .then(() => response.send({ message: "注册成功" }))
-    .catch(error => response.send(error));
 };
 
 module.exports = {
