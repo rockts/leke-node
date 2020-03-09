83e4e039bf2504dcd0a14e6ef78c14adf860069c
rockts <rockts@sina.com>
2020-03-10 00:47:32 +0800

    验证与解码 JWT

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ const secret = "I_LOVE_LEKE";
 
 const token = jwt.sign(payload, secret);
 console.log(token);
+
+jwt.verify(token, secret, (error, decoded) => {
+  if (error) {
+    console.log(error.message);
+    return;
+  }
+
+  console.log(decoded);
+});
