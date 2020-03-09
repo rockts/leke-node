834d0d320da6605f638c5f97f24f6f1664504f52
rockts <rockts@sina.com>
2020-03-10 05:04:50 +0800

    上传多个文件

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.post("/profile", upload.single("avatar"), (request, response, next) => {
   response.send(request.file);
 });
 
+app.post(
+  "/photos/upload",
+  upload.array("photos", 3),
+  (request, response, next) => {
+    response.send(request.files);
+  }
+);
+
 app.listen(8080, () => {
   console.log("localhost:8080");
 });
